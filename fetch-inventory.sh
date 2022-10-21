5e7742e8b1546b8ea8497fd4b035e21a493da67e
lluisitu <114317707+lluisitu@users.noreply.github.com>
2022-10-21 07:37:58 -0500

    Update fetch-inventory.sh

--- fetch-inventory.sh
+++ fetch-inventory.sh
@@ -18,7 +18,7 @@ do
       "pagestart": 0,
       "filter": {
           "year": [2022,2023], 
-          "series": ["tundra"], 
+          "series": ["*tundra*"], 
           "dealers": ["'$i'"],
           "andfields": ["accessory", "packages", "dealer"]
         }
